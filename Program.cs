770afce9ecb9db17075641b3ed20a0feff569894
primrosies <provesialine@gmail.com>
2025-10-26 16:37:06 -0300

    Calculadora simples iniciada

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Choose a number: ");
+int userFirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Choose a second number: ");
+int userSecondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The sum is:" + " " + (userFirstNumber + userSecondNumber));
